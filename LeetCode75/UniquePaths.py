b550444d780cccf880e422dca0551523fe1fa730
Alex Zeng <alex.zenggg@gmail.com>
2024-05-21 11:39:36 -0400

    unique paths

---
+++ LeetCode75/UniquePaths.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        dp = [[0 for a in range(n + 1)] for b in range(m + 1)]
+
+        dp[m][n - 1] = 1
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1]
+
+        return dp[0][0]
