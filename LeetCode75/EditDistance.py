cab650ae405362699bc08b1c2f6ca69b1b04a6f5
Alex Zeng <alex.zenggg@gmail.com>
2024-10-01 10:22:28 -0400

    edit distance

---
+++ LeetCode75/EditDistance.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        
+        n, m = len(word1), len(word2)
+
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+        for j in range(m + 1):
+            dp[n][j] = m - j
+
+        for i in range(n + 1):
+            dp[i][m] = n - i
+
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1])
+
+        return dp[0][0]
