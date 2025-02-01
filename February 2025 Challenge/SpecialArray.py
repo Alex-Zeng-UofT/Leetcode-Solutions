e7a81efb097a93e4969996b1112109b04a75be48
Alex-Zeng-UofT <alex.zenggg@gmail.com>
2025-02-01 18:24:02 -0500

    feb 1st 2025

---
+++ February 2025 Challenge/SpecialArray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+
+        for i in range(1, len(nums)):
+            if (nums[i] + nums[i - 1]) % 2 != 1:
+                return False
+
+        return True
+        
